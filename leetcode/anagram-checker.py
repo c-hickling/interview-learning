43a89f6444b7f6f3a91b85388552fd054eb52080
cmh <you@example.com>
2026-04-19 21:47:20 +0100

    Anagram Checker Leetcode

--- leetcode/anagram-checker.py
+++ leetcode/anagram-checker.py
@@ -2,22 +2,21 @@
 
 from collections import defaultdict
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+def isAnagram(self, s: str, t: str) -> bool:
 
-    # An anagram is a word or phrase formed by rearranging the letters of a different 
-    # word or phrase, typically using all the original letters exactly once.    
-        if len(s) != len(t):
+# An anagram is a word or phrase formed by rearranging the letters of a different 
+# word or phrase, typically using all the original letters exactly once.    
+    if len(s) != len(t):
+        return False
+
+    letter_counts = defaultdict(int)
+
+    for letter in s:
+        letter_counts[letter] += 1
+
+    for letter in t:
+        letter_counts[letter] -= 1
+        if letter_counts[letter] < 0:
             return False
 
-        letter_counts = defaultdict(int)
-
-        for letter in s:
-            letter_counts[letter] += 1
-
-        for letter in t:
-            letter_counts[letter] -= 1
-            if letter_counts[letter] < 0:
-                return False
-
-        return True
+    return True
